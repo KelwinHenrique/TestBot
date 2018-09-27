af7e2e2610aa707a28060062ccffcfbb566b6462
Kelwin <kelwin-hike@hotmail.com>
2018-09-26 21:52:10 -0300

    .

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const PORT = process.env.PORT || 5000
 
 express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  router.use(express.static(path.resolve(__dirname, 'client')))
+  var messages = []
+  var sockets = []
+  
+  
+  router.get('/webhook', function (req, res){
+      res.send('ok')
+  })
